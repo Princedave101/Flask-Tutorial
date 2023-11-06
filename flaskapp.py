04161eb5b0d25b787b0c62429cbebd8d8b421636
PrinceDave101 <dpokeke@gmail.com>
2023-11-06 19:39:47 +0100

    Added a contact route

--- flaskapp.py
+++ flaskapp.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/contact")
+def contactPage():
+    return "u can contact us at fuck u by ur self"
 
 if __name__ == "__main__":
     app.run(debug=True)
